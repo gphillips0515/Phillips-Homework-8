0443df3c7c268b537beb1039d4cf822a008ad279
Grace Phillips <g.phillips@setonhill.edu>
2020-04-08 20:13:52 -0400

    Initial Commit

---
+++ Phillips Homework 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Phillips Homework 8
+//
+//  Created by Grace Phillips on 4/8/20.
+//  Copyright © 2020 Grace Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
